9ceef41a0ca0bf014eb242ed314e09b4bbad4e95
Pramod Ghodke <88473084+pramod-ghodke@users.noreply.github.com>
2022-08-30 23:55:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Menubar from "./components/Menubar";
+import Payment from "./components/Payment";
+
+function App() {
+  return (
+    <div className="App">
+      <Menubar />
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
